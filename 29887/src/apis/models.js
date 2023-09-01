e19db5083dc8a2b0fa90ef40e365fa90d20dbdd1
alekseevnagirl <vI5iD5pT9zI7>
2023-09-01 14:30:03 +0400

    delete

--- 29887/src/apis/models.js
+++
@@ -1,6 +0,0 @@
-const baseURL = process.env.VUE_APP_JSON_SERVICE_URL;
-
-export const fetchPosts = async () => {
-  const response = await fetch(`${baseURL}`);
-  return response.json();
-};
--- 29887/src/validators/uniqueTasks.js
+++
@@ -1,7 +0,0 @@
-export function isUniqueTask(value, tasks) {
-  const findTask = tasks.find(element => element.title === value);
-  if (findTask !== undefined) {
-    return '';
-  }
-  return value;
-}
